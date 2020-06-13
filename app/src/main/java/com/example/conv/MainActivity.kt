45be5e01460bfe4510256eb53dc13897d2fdbf94
nazim <nazimmalyshev47@gmail.com>
2020-06-14 01:56:05 +0300

    UPDATE! input is checked >_<

--- app/src/main/java/com/example/conv/MainActivity.kt
+++ app/src/main/java/com/example/conv/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.conv
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+fun check(str: String): Boolean{
+    if(str == "") return false
+    for(i in str){
+        if( (i.toChar()<'0') or (i.toChar()>'9') ) return false;
+    }
+    return true
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,51 +27,52 @@ class MainActivity : AppCompatActivity() {
         rub2.setOnClickListener { sd.text = "RUB" }
         uah2.setOnClickListener { sd.text = "UAH" }
         calc.setOnClickListener {
-            var n = cnt.text.toString().toDouble()
-            if(fs.text.toString() == "USD"){
-                if(sd.text.toString() == "USD"){
-                    n *= 1
-                } else if(sd.text.toString() == "EUR"){
-                    n*=0.88
-                } else if(sd.text.toString() == "RUB"){
-                    n*=68
-                } else if(sd.text.toString() == "UAH"){
-                    n*=24.7
+            if (check(cnt.text.toString())) {
+                var n = cnt.text.toString().toDouble()
+                if(fs.text.toString() == "USD") {
+                    if (sd.text.toString() == "USD") {
+                        n *= 1
+                    } else if (sd.text.toString() == "EUR") {
+                        n *= 0.88
+                    } else if (sd.text.toString() == "RUB") {
+                        n *= 68
+                    } else if (sd.text.toString() == "UAH") {
+                        n *= 24.7
+                    }
+                } else if (fs.text.toString() == "EUR") {
+                    if (sd.text.toString() == "USD") {
+                        n *= 1.13
+                    } else if (sd.text.toString() == "EUR") {
+                        n *= 1
+                    } else if (sd.text.toString() == "RUB") {
+                        n *= 78
+                    } else if (sd.text.toString() == "UAH") {
+                        n *= 30
+                    }
+                } else if (fs.text.toString() == "RUB") {
+                    if (sd.text.toString() == "USD") {
+                        n *= 0.014
+                    } else if (sd.text.toString() == "EUR") {
+                        n *= 0.012
+                    } else if (sd.text.toString() == "RUB") {
+                        n *= 1
+                    } else if (sd.text.toString() == "UAH") {
+                        n *= 0.39
+                    }
+                } else if (fs.text.toString() == "UAH") {
+                    if (sd.text.toString() == "USD") {
+                        n *= 0.375
+                    } else if (sd.text.toString() == "EUR") {
+                        n *= 0.03
+                    } else if (sd.text.toString() == "RUB") {
+                        n *= 2.6
+                    } else if (sd.text.toString() == "UAH") {
+                        n *= 1
+                    }
                 }
-            } else if(fs.text.toString() == "EUR"){
-                if(sd.text.toString() == "USD"){
-                    n*=1.13
-                } else if(sd.text.toString() == "EUR"){
-                    n*=1
-                } else if(sd.text.toString() == "RUB"){
-                    n*=78
-                } else if(sd.text.toString() == "UAH"){
-                    n*=30
-                }
-            } else if(fs.text.toString() == "RUB"){
-                if(sd.text.toString() == "USD"){
-                    n*=0.014
-                } else if(sd.text.toString() == "EUR"){
-                    n*=0.012
-                } else if(sd.text.toString() == "RUB"){
-                    n*=1
-                } else if(sd.text.toString() == "UAH"){
-                    n*=0.39
-                }
-            } else if(fs.text.toString() == "UAH"){
-                if(sd.text.toString() == "USD"){
-                    n*=0.375
-                } else if(sd.text.toString() == "EUR"){
-                    n*=0.03
-                } else if(sd.text.toString() == "RUB"){
-                    n*=2.6
-                } else if(sd.text.toString() == "UAH"){
-                    n*=1
-                }
-            }
 
-            res.text = n.toString()
-
+                res.text = n.toString()
+            } else res.text = "ERROR INPUT"
         }
     }
 }
